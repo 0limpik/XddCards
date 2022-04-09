bcde2953df09419e673916a16230a2fafe66e5f7
0limpik <olimpik.net@gmail.com>
2022-04-09 06:16:54 +0300

    create build branch

--- Assets/Source/Scripts/UI/BlackJack/ICycleRequired.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xdd.Model.Cycles.BlackJack;
-
-namespace Xdd.Scripts.UI.BlackJack
-{
-    internal interface ICycleRequired
-    {
-        void InitCycle(BJCycle cycle, User user);
-    }
-}
